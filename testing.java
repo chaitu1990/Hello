007f62610dc281b1fe6a8c233537ea37e7f472b0
chaitu1990 <chaitalisanap2010@gmail.com>
2020-07-20 09:49:23 +0530

    Add files via upload
    Testing

---
+++ testing.java
@@ -0,0 +1,12 @@
+public class testing
+ {
+
+public static void main (String[] args) {
+        
+      int num1 = 10, 
+	num2 = 15, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
+}
