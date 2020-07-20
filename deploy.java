435eafab3454f9bf888b9010f0c544b11a51c7e9
chaitu1990 <chaitalisanap2010@gmail.com>
2020-07-20 09:57:25 +0530

    deploy

---
+++ deploy.java
@@ -0,0 +1,12 @@
+public class deploy
+ {
+
+public static void main (String[] args) {
+        
+      int num1 = 30, 
+	num2 = 15, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
+}
